5aec1414eec2b1c159cbd233d86d2033e1b3363b
GabrielChiarelli <102618272+GabrielChiarelli@users.noreply.github.com>
2024-01-08 11:47:21 -0300

    Aula 05

--- pong-na-godot/scenes/bola/bola.gd
+++ pong-na-godot/scenes/bola/bola.gd
@@ -1,10 +1,14 @@
 extends Area2D
 
 
+# Parâmetros
 var velocidade_da_bola : int = 500
 var posicao_inicial : Vector2 = Vector2(640, 360)
 var direcao_inicial : Vector2 = Vector2(0, 0)
 var nova_direcao : Vector2 = Vector2(0, 0)
+
+var y_minimo : float = 0
+var y_maximo : float = 720
 
 
 func _ready():
@@ -13,6 +17,7 @@ func _ready():
 
 func _process(delta):
 	movimentar_bola(delta)
+	colidir_com_as_paredes()
 	
 
 func resetar_bola() -> void:
@@ -34,16 +39,24 @@ func escolher_direcao_inicial() -> void:
 func movimentar_bola(delta : float) -> void:
 	position += nova_direcao * velocidade_da_bola * delta
 	
+
+func colidir_com_as_paredes() -> void:
+	# Manda a Bola na direção contrária ao tentar sair da tela
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	#if position.y >= y_maximo:
+	#	nova_direcao.y *= -1
+	#elif position.y <= y_minimo:
+	#	nova_direcao.y *= -1
+		
+	if position.y >= y_maximo or position.y <= y_minimo:
+		nova_direcao.y *= -1
+		
+	# +1 * +1 = +1
+	# +1 * -1 = -1
+	# -1 * -1 = +1	
+
+
+func _on_body_entered(body):
+	# Manda a Bola na direção contrária ao colidir com os jogadores
+	if body.is_in_group("jogadores"):
+		nova_direcao.x *= -1
