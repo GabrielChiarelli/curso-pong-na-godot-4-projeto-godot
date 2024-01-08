3b9b6d66b2141e020aa918af4434b2031b87ba5e
GabrielChiarelli <102618272+GabrielChiarelli@users.noreply.github.com>
2024-01-08 15:50:25 -0300

    Aula 07

--- pong-na-godot/scenes/game_manager/game_manager.gd
+++ pong-na-godot/scenes/game_manager/game_manager.gd
@@ -1,14 +1,16 @@
 extends Node2D
 
 
+# Bola
 @onready var bola : Area2D = $"../Bola"
 
+# Pontuações
 var pontuacao_jogador1 : int = 0
 var pontuacao_jogador2 : int = 0
 
-
-func _ready():
-	pass
+# UI
+@onready var texto_pontuacao_jogador1 : Label = $"../UI/PainelDePontuacao/TextoPontuacaoJogador1"
+@onready var texto_pontuacao_jogador2 : Label = $"../UI/PainelDePontuacao/TextoPontuacaoJogador2"
 
 
 func _process(delta):
@@ -17,9 +19,11 @@ func _process(delta):
 
 func _on_gol_1_area_entered(area):
 	pontuacao_jogador2 += 1
+	texto_pontuacao_jogador2.text = str(pontuacao_jogador2)
 	bola.rodar_timer()
 
 
 func _on_gol_2_area_entered(area):
 	pontuacao_jogador1 += 1
+	texto_pontuacao_jogador1.text = str(pontuacao_jogador1)
 	bola.rodar_timer()
